3d2036aedcf7f3bbe80ff44c01f4c609d0934b40
LynnLo30 <lynn.lo3005@gmail.com>
2025-09-16 16:47:01 +0800

    feat: add HTML layout, base CSS, and override Bootstrap variables.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,105 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="calculator-box col-6 card p-5">
+        <div class="bmi-header mb-3">
+          <h3 class="title">身體質量指數 (BMI) 計算器</h3>
+          <p class="description">世界衛生組織(WHO)指出以身高與體重計算出的身體質量指數(Body Mass
+            Index，簡稱BMI)是最常用於評估體位的一項簡易測量指標。</p>
+        </div>
+        <form class="calculator-form">
+          <div class="mb-3">
+            <label for="height" class="form-label">身高（公分/ cm）</label>
+            <input type="number" class="form-control" id="height" min="40" max="272"
+              placeholder="請輸入身高">
+          </div>
+          <div class="mb-5">
+            <label for="weight" class="form-label">體重（公斤/ kg）</label>
+            <input type="number" class="form-control" id="weight" min="2" max="635"
+              placeholder="請輸入體重">
+          </div>
+          <div class="actions-control mx-auto">
+            <button type="submit" class="btn btn-primary mx-3">開始計算</button>
+            <button type="reset" class="btn btn-secondary mx-3">全部清除</button>
+          </div>
+        </form>
+      </div>
+      <div class="result-box col-4 d-flex flex-column justify-content-between">
+        <div class="grade-card">
+          <div class="underweight card">
+            <div class="card-body row align-items-center mx-1">
+              <p class="card-title col">BMI<span class="numberStyle"> &lt; 18.5</span></p>
+              <p class="card-text col text-center">體重過輕</p>
+            </div>
+          </div>
+          <div class="normalWeight card">
+            <div class="card-body row align-items-center mx-1">
+              <p class="card-title col"><span class="numberStyle">18.5 &le; </span>BMI<span
+                  class="numberStyle"> &lt; 24</span>
+              </p>
+              <p class="card-text col text-center">健康體重</p>
+            </div>
+          </div>
+          <div class="overweight card">
+            <div class="card-body row align-items-center mx-1">
+              <p class="card-title col"><span class="numberStyle">24 &le; </span>BMI<span
+                  class="numberStyle"> &lt; 27</span>
+              </p>
+              <p class="card-text col text-center">體重過重</p>
+            </div>
+          </div>
+          <div class="obesity card">
+            <div class="card-body row align-items-center mx-1">
+              <p class="card-title col">BMI<span class="numberStyle"> &ge; 27</span></p>
+              <p class="card-text col text-center">肥胖</p>
+            </div>
+          </div>
+        </div>
+        <figure class="caption-text">
+          <p class="text-secondary">18歲（含）以上成人BMI範圍值及體重對照表</p>
+          <figcaption class="blockquote-footer">
+            資料來源： <cite title="Source Title">衛生福利部國民健康署</cite>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+  // * {
+  //   outline: 1px solid salmon;
+  // }
+
+  .numberStyle {
+    font-family: "Noto Sans Math", sans-serif;
+    font-size: 1.22rem;
+  }
+
+  .actions-control {
+    width: fit-content;
+  }
+
+  .grade-card {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 65vh;
+
+    .card {
+      min-height: 4.5rem;
+
+      .card-title {
+        margin: 0;
+      }
+    }
+  }
+
+  .caption-text,
+  .caption-text>* {
+    margin: 0;
+    font-size: 0.8rem;
+  }
+</style>
